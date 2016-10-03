b4abfeb54496cc7eaf9028937a24fcaf9e71ae25
Vijaya Gowri Potturu <vpotturu@corp.paradigmit.com>
2016-10-03 16:26:10 +0530

    map added

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,15 +8,27 @@
  * Controller of the angularappApp
  */
 angular.module('angularappApp')
-  .controller('MainCtrl', ['$scope','weatherService','$http',function ($scope, weatherService, $http) {
-   
-   $scope.getWeatherData = function(){
-    var cat = weatherService.getweatherApi();
-      cat.then(function(data) {
-          $scope.categories = data.data;
-          console.log(' $scope.categories', $scope.categories)
-      })
+  .controller('MainCtrl', ['$scope','weatherService','$http', function ($scope, weatherService, $http) {
 
-    }
+  $scope.getWeatherData = function (name) {
+        weatherService.getweatherApi(name).success(function (response) {
+          $scope.cities = response;
+          console.log('$scope.cities',$scope.cities)
+        }).error(function (error) {
+           console.log('error',error);
+        });
+  };
+  if(localStorage.hasOwnProperty('$scope.cities')){
+  $scope.getCities =  localStorage.getItem($scope.cities);
+  console.log('$scope.getCities')
+  }else{
+   $scope.getCities = localStorage.setItem('$scope.cities', JSON.stringify($scope.cities));
+  }
+  $scope.points = [
+        { "name": "Canberra", "latitude": -35.282614, "longitude": 149.127775 },
+        { "name": "Melbourne", "latitude": -37.815482, "longitude": 144.983460 },
+        { "name": "Sydney", "latitude": -33.869614, "longitude": 151.187451 },
+        { "name": "India", "latitude": 20.5937, "longitude": 78.9629 }
+    ];
 
   }]);
--- app/scripts/service.js
+++ app/scripts/service.js
@@ -4,16 +4,9 @@ angular.module('angularappApp').factory('weatherService', ['$http', '$rootScope'
   var base_url = 'http://api.openweathermap.org';
   //id =  c46b536c57f49065c4e4825b136f8614;
 
-    service.getweatherApi = function () {
-    return $http.get(base_url + '/data/2.5/weather?q=London,uk'+'&appid=c46b536c57f49065c4e4825b136f8614').then(function(data){
-        return data;
-        //console.log('data',data)
-    })
+    service.getweatherApi = function (name) {
+    return $http.get(base_url + '/data/2.5/weather?q='+name+'&appid=c46b536c57f49065c4e4825b136f8614')
+   
   };
 return service;
-
 }]);
-
-//http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=c46b536c57f49065c4e4825b136f8614
-
-//http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=c46b536c57f49065c4e4825b136f8614
