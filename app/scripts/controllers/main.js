b4231c595fee783686ea46ea0d2305db57a4db62
Vijaya Gowri Potturu <vpotturu@corp.paradigmit.com>
2016-10-04 17:47:41 +0530

    df

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,27 +8,40 @@
  * Controller of the angularappApp
  */
 angular.module('angularappApp')
-  .controller('MainCtrl', ['$scope','weatherService','$http', function ($scope, weatherService, $http) {
+  .controller('MainCtrl', ['$scope', 'weatherService', '$http', function ($scope, weatherService, $http) {
+    $scope.cities = '';
+    $scope.citiData = [];
 
-  $scope.getWeatherData = function (name) {
-        weatherService.getweatherApi(name).success(function (response) {
-          $scope.cities = response;
-          console.log('$scope.cities',$scope.cities)
-        }).error(function (error) {
-           console.log('error',error);
-        });
-  };
-  if(localStorage.hasOwnProperty('$scope.cities')){
-  $scope.getCities =  localStorage.getItem($scope.cities);
-  console.log('$scope.getCities')
-  }else{
-   $scope.getCities = localStorage.setItem('$scope.cities', JSON.stringify($scope.cities));
-  }
-  $scope.points = [
-        { "name": "Canberra", "latitude": -35.282614, "longitude": 149.127775 },
-        { "name": "Melbourne", "latitude": -37.815482, "longitude": 144.983460 },
-        { "name": "Sydney", "latitude": -33.869614, "longitude": 151.187451 },
-        { "name": "India", "latitude": 20.5937, "longitude": 78.9629 }
+    $scope.getWeatherData = function (name) {
+      weatherService.getweatherApi(name).success(function (response) {
+        $scope.cities = response;
+        var a = {};
+        a.langitude = $scope.cities.coord.lon;
+        a.lattitude = $scope.cities.coord.lat;
+        a.weatherDescription = $scope.cities.weather;
+        for (var i = 0; i <= a.weatherDescription; i++) {
+          a.description = a.weatherDescription[i].description;
+          console.log('a.description',a.description)
+        }
+        $scope.citiData.push(a);
+        if (localStorage.hasOwnProperty('test')) {
+          $scope.citieNames = localStorage.getItem("test");
+        } else {
+          $scope.citieNames = localStorage.setItem("test", JSON.stringify($scope.citiData));
+        }
+        console.log('$scope.cities', $scope.cities)
+      }).error(function (error) {
+        console.log('error', error);
+      });
+    };
+
+    $scope.points = [
+      { "name": "Canberra", "latitude": -35.282614, "longitude": 149.127775 },
+      { "name": "Melbourne", "latitude": -37.815482, "longitude": 144.983460 },
+      { "name": "Sydney", "latitude": -33.869614, "longitude": 151.187451 },
+      { "name": "India", "latitude": 20.5937, "longitude": 78.9629 }
     ];
+
+
 
   }]);
--- app/scripts/route.js
+++ app/scripts/route.js
@@ -1,16 +1,16 @@
-  angular.module('angularappApp').config(function ($routeProvider) {
-    $routeProvider
-      .when('/main', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/main'
-      });
-  });
+angular.module('angularappApp').config(function ($routeProvider) {
+  $routeProvider
+    .when('/main', {
+      templateUrl: 'views/main.html',
+      controller: 'MainCtrl',
+      controllerAs: 'main'
+    })
+    .when('/about', {
+      templateUrl: 'views/about.html',
+      controller: 'AboutCtrl',
+      controllerAs: 'about'
+    })
+    .otherwise({
+      redirectTo: '/main'
+    });
+});
